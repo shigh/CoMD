9d6bc5d0fb3618798ae33ece07f727d85c472cf8
shigh <highscot@gmail.com>
2015-04-13 17:06:56 -0500

    Simple strong scaling result.

--- scale/mpi-strongScaling.sh
+++ scale/mpi-strongScaling.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
 # Simple strong scaling study with eam potential and 256,000 atoms
-aprun -n 1  ../bin/CoMD-mpi -e -i 1 -j 1 -k 1 -x 40 -y 40 -z 40
-aprun -n 2  ../bin/CoMD-mpi -e -i 2 -j 1 -k 1 -x 40 -y 40 -z 40
-aprun -n 4  ../bin/CoMD-mpi -e -i 2 -j 2 -k 1 -x 40 -y 40 -z 40
-aprun -n 8  ../bin/CoMD-mpi -e -i 2 -j 2 -k 2 -x 40 -y 40 -z 40
+# aprun -n 1  ../bin/CoMD-mpi -e -i 1 -j 1 -k 1 -x 40 -y 40 -z 40
 aprun -n 16 ../bin/CoMD-mpi -e -i 4 -j 2 -k 2 -x 40 -y 40 -z 40
+aprun -n 32 ../bin/CoMD-mpi -e -i 4 -j 4 -k 2 -x 40 -y 40 -z 40
+aprun -n 64 ../bin/CoMD-mpi -e -i 4 -j 4 -k 4 -x 40 -y 40 -z 40
