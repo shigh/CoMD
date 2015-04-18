1e916faad73d732ac6351b282775ab53e57b8eeb
shigh <highscot@gmail.com>
2015-04-18 16:44:02 -0500

    Strong scaling.

--- scale/mpi-strongScaling.sh
+++ scale/mpi-strongScaling.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-#PBS -l nodes=4:ppn=32:xe
+#PBS -l nodes=256:ppn=32:xe
 #PBS -l walltime=00:20:00
 #PBS -N mpi-strongscaling
 
@@ -7,8 +7,12 @@ TD='--temp 0 --delta 0'
 N=16
 X=160
 
+cd ${PBS_O_WORKDIR}
+
 # Simple strong scaling study with eam potential and 256,000 atoms
 # aprun -n 1  ../bin/CoMD-mpi -e -i 1 -j 1 -k 1 -x 40 -y 40 -z 40
-aprun -n 1 ../bin/CoMD-mpi -e -i 1 -j 1 -k 1 -x $X -y $X -z $X $TD
-aprun -n 8 ../bin/CoMD-mpi -e -i 2 -j 2 -k 2 -x $X -y $X -z $X $TD
+#aprun -n 1 ../bin/CoMD-mpi -e -i 1 -j 1 -k 1 -x $X -y $X -z $X $TD
+#aprun -n 8 ../bin/CoMD-mpi -e -i 2 -j 2 -k 2 -x $X -y $X -z $X $TD
 aprun -N $N -n 64 ../bin/CoMD-mpi -e -i 4 -j 4 -k 4 -x $X -y $X -z $X $TD
+aprun -N $N -n 512 ../bin/CoMD-mpi -e -i 8 -j 8 -k 8 -x $X -y $X -z $X $TD
+aprun -N $N -n 4096 ../bin/CoMD-mpi -e -i 16 -j 16 -k 16 -x $X -y $X -z $X $TD
