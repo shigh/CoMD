0d94f8ec3b88d6672aecf9c5011f26af6c3bd5e9
ekmolloy <ekmolloy@uchicago.edu>
2015-04-18 22:57:53 -0500

    updated script

--- forceLJ/baseline/plot.py
+++ forceLJ/baseline/plot.py
@@ -9,6 +9,10 @@ t = X[:,1]
 
 nAtoms = n * n * n * 4.0
 
+# fit data
+m, b = np.polyfit(np.log10(nAtoms),np.log10(t),1)
+x = 10**np.arange(15)
+
 # Plots
 fig = plt.figure(figsize=(8.0, 4.25))
 fig.suptitle("Computation of Lenard-Jones Force", fontsize=14)
@@ -17,7 +21,8 @@ ax1 = plt.subplot(gs[0, 0])
 ax2 = plt.subplot(gs[0, 1])
 
 ax1.loglog(nAtoms, t, 'ko')
-ax1.set_xlim(1e3, 1e7)
+ax1.loglog(x, (10**b)*(x**m), 'r-', label="")
+#ax1.set_xlim(1e3, 1e7)
 ax1.set_xlabel("# of atoms")
 ax1.set_ylabel("time (s)")
 
--- forceLJ/baseline/runbw.sh
+++ forceLJ/baseline/runbw.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #PBS -l nodes=1:ppn=1:xe
-#PBS -l walltime=01:00:00
+#PBS -l walltime=04:00:00
 #PBS -N fLJ_nAtoms
 #PBS -e $PBS_JOBID.err
 #PBS -o $PBS_JOBID.out
@@ -9,11 +9,20 @@
 
 cd $PBS_O_WORKDIR
 
-for ((n=10; n<=100; n+=10))
+#for ((n=10; n<=100; n+=10))
+#do
+#    aprun -n 1 ../../../bin/CoMD-mpi \
+#        --nx ${n} --ny ${n} --nz ${n} \
+#        --xproc 1 --yproc 1 --zproc 1 \
+#        --nSteps 20 --printRate 5 --dt 1 \
+#        --lat 3.615 --temp 0 --delta 0 > fLJ_nx${n}_ny${n}_nz${n}.txt
+#done
+
+for ((n=100; n<=750; n+=250))
 do
     aprun -n 1 ../../../bin/CoMD-mpi \
         --nx ${n} --ny ${n} --nz ${n} \
         --xproc 1 --yproc 1 --zproc 1 \
-        --nSteps 20 --printRate 5 --dt 1 \
+        --nSteps 1 --printRate 5 --dt 1 \
         --lat 3.615 --temp 0 --delta 0 > fLJ_nx${n}_ny${n}_nz${n}.txt
 done
