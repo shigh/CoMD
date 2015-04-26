6efccd3bd76a76c2cfcb0d0cc1e00f2130d1799e
Erin <molloy.erin.k@gmail.com>
2015-04-26 12:25:51 -0500

    adding optim results

---
+++ performance/forceLJ/optim/cc/runbw.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+#PBS -l nodes=1:ppn=1:xe
+#PBS -l walltime=02:00:00
+#PBS -N fLJ_cc_opt
+#PBS -e $PBS_JOBID.err
+#PBS -o $PBS_JOBID.out
+#PBS -m bea
+#PBS -M emolloy2@illinois.edu
+
+cd $PBS_O_WORKDIR
+
+for ((n=10; n<=100; n+=10))
+do
+    ../../../../bin/CoMD-serial-cc-opt \
+        --potDir ../../../../pots \
+        --nx ${n} --ny ${n} --nz ${n} \
+        --nSteps 20 --printRate 5 --dt 1 \
+        --lat 3.615 --temp 0 --delta 0 > fLJ_nx${n}_ny${n}_nz${n}.txt
+done
+
+#for n in 100
+#do
+#    ../../../../bin/CoMD-serial-cc-opt \ 
+#        --potDir ../../../../pots \
+#        --nx ${n} --ny ${n} --nz ${n} \
+#        --xproc 1 --yproc 1 --zproc 1 \
+#        --nSteps 20 --printRate 5 --dt 1 \
+#        --lat 3.615 --temp 0 --delta 0 > fLJ_nx${n}_ny${n}_nz${n}.txt
+#done
